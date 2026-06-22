600f8391a14f9055c7d5c4553e97afe11b08506d
Haowei Hsu <hwhsu1231@localizethedocs.org>
2026-06-22 15:34:36 +0800

    ✨ feat(init): initialize branch 'main'
    Initialize the 'main' branch with the following files:
    
    - Add document files: README.md, LICENSE-BSD, LICENSE-CC.
    - Add GitHub workflow files: ci-check-pr-status,..., etc.
    - Add GitHub issue templates: language_request.yml,..., etc.
    - Add Git config files: .gitmodules, .gitignore.
    - Add Git submodules: cmake/modules, cmake/plugins.
    - Add CMake script files: CMakeLists.txt, cmake/**/* files.
    - Add config files: versions.json, languages.json, crowdin.yml.

---
+++ cmake/custom/ltd-config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",  "Development"],
+    ["stable",  "Stable Release"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/xonsh-shell-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/xonsh-shell-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/xonsh-shell-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/xonsh-shell-docs-l10n"],
+  ]
+};
